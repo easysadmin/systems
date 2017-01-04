107e198fa1148723b25baed0a8724d756c1b6dfc
Ivan Alejandro <ialejandro@syspixel.com>
2017-01-04 10:15:19 +0100

    New Nagios check for file SFTP-HDFS

---
+++ monitoring/check_file_sftp_hdfs.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#------------------------------------------------------------------------------
+# [Syspixel] Check File SFTP HDFS
+#
+# 
+#
+# TODO
+# 
+#------------------------------------------------------------------------------
+VERSION=0.1
+
+# Exit codes
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+
+# Print help
+_usage() {
+echo "Usage: check_file_sftp_hdfs.sh [-h help] -H <host> -f <file>
+
+  -h		Print this help message
+  -H		Host where's log
+  -f		Path log's file to read result
+  
+  Exit status:
+  0  if OK
+  1  if minor problems (e.g., cannot create a temp file)
+  2  if serious trouble (e.g., cannot access command-line argument)
+  3  unknown
+  "
+}
+
+# Arguments
+while getopts ":H:f:h" opt; do
+	case $opt in
+		h) _usage; exit $STATE_OK;;
+		H) HOST=$OPTARG;;
+		f) FILE=$OPTARG;;
+		\?) echo "Invalid option: -$OPTARG" >&2; _usage; exit $STATE_CRITICAL;;
+		:) echo "Requiere an argument: -$OPTARG" >&2; _usage; exit $STATE_CRITICAL;;
+	esac
+done
+
+# Check arguments
+if [[ -z "$HOST" || -z "$FILE" ]]; then
+        echo "Empty obligatory arguments"
+        _usage
+        exit $STATE_WARNING;
+fi
+
+# Vars
+SSH="ssh $HOST"
+RESULT=$($SSH tail -1 "$FILE" | awk '{ print $2 }' | sed 's/://')
+
+# Main #####################################################
+
+if [[ -z "$RESULT" ]]; then
+	echo "UNKNOWN"
+	exit $STATE_UNKNOWN
+fi
+
+if [[ "$RESULT" == "INFO" ]]; then
+	echo "UPLOAD WEBORAMA DATA OK - COMPLETED"
+	exit $STATE_OK
+else
+	echo "UPLOAD WEBORAMA DATA FAILED - ERROR"
+	exit $STATE_CRITICAL
+fi
