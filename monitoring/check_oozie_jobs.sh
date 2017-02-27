680e1301208f25f39a97b48ec99a729c94f82005
Ivan Alejandro <ialejandro@syspixel.com>
2017-02-27 13:54:52 +0100

    Jobs amount to check

--- monitoring/check_oozie_jobs.sh
+++ monitoring/check_oozie_jobs.sh
@@ -5,10 +5,9 @@
 #
 # 
 #
-# TODO
 # 
 #------------------------------------------------------------------------------
-VERSION=0.1
+VERSION=1.0
 
 # Exit codes
 STATE_OK=0
@@ -18,12 +17,13 @@ STATE_UNKNOWN=3
 
 # Print help
 _usage() {
-echo "Usage: check_oozie_jobs.sh [-h help] -H <host> -U <url> -A <name-job>
+echo "Usage: check_oozie_jobs.sh [-h help] -H <host> -U <url> -A <name-job> [-l len-jobs]
 
   -h		Print this help message
   -H		Host where is oozie
   -U		URL oozie endpoint
   -A		Name of job (App Name)
+  -l		Jobs amount to check. Default: 1.
   
   Exit status:
   0  if OK
@@ -34,12 +34,15 @@ echo "Usage: check_oozie_jobs.sh [-h help] -H <host> -U <url> -A <name-job>
 }
 
 # Arguments
-while getopts ":H:U:A:h" opt; do
+while getopts ":H:U:A:l:h" opt; do
 	case $opt in
 		h) _usage; exit $STATE_OK;;
 		H) HOST=$OPTARG;;
 		U) URL=$OPTARG;;
 		A) NAME=$OPTARG;;
+		l) if [[ ! -z "$OPTARG" ]]; then
+			LEN=$OPTARG
+		   fi;;
 		\?) echo "Invalid option: -$OPTARG" >&2; _usage; exit $STATE_CRITICAL;;
 		:) echo "Requiere an argument: -$OPTARG" >&2; _usage; exit $STATE_CRITICAL;;
 	esac
@@ -49,19 +52,50 @@ done
 if [[ -z "$HOST" || -z "$URL" || -z "$NAME" ]]; then
         echo "Empty obligatory arguments"
         _usage
-        exit $STATE_WARNING;
+        exit $STATE_WARNING
+elif [[ -z "$LEN" ]]; then
+	LEN=1
 fi
+
+# Comprobe if jobs come from various workflows
+function _checkjobs {
+	for i in `seq 1 $(expr $LEN "*" 3)`; do # 3 => amounts of stats to show (status, date created and time created)
+		if ! (( $i % 3 )); then
+			STAT1=$(echo $1 | cut -d" " -f$(expr $i - 2))
+			POS1=$(expr $i - 2)
+			POS2=$(expr $i - 1)
+			if [[ "$STAT1" == "SUCCEEDED" || "$STAT1" == "RUNNING" ]]; then
+				STATUS=$(echo $1 | cut -d" " -f$POS1,$POS2,$i)
+			else
+				STATUS=$(echo $1 | cut -d" " -f$POS1,$POS2,$i)
+				echo "$STATUS"
+				break
+			fi
+		fi
+	done
+
+	# If done without problems, correct response
+	echo "$STATUS"
+}
 
 # Vars
 SSH="ssh $HOST"
-RESULT=$($SSH "oozie jobs -oozie $URL -localtime -verbose -len 1 -filter name=$NAME" | awk '{ print $14 }' | sed '1d;$d;/^\s*$/d')
+JOBS=$($SSH "oozie jobs -oozie $URL -localtime -verbose -len $LEN -filter name=$NAME" | awk '{ print $14, $15, $16 }' | sed '1d;$d;/^\s*$/d')
+STATUS=$(_checkjobs "$JOBS")
+RESULT=$(echo $STATUS | awk '{ print $1 }')
+DESC=$(echo $STATUS | awk '{ print $1 " (Started: " $2, $3 ")" }')
+
 
 # Main #####################################################
+if [[ -z "$RESULT" ]]; then
+	echo "Don't found name's job"
+	exit $STATE_CRITICAL
+fi
 
 if [[ "$RESULT" == "SUCCEEDED" || "$RESULT" == "RUNNING" ]]; then
-	echo "OOZIE'S JOB $NAME OK - $RESULT"
+	echo "OOZIE'S JOB $NAME OK - $DESC"
 	exit $STATE_OK
 else
-	echo "OOZIE'S JOB $NAME FAILED - $RESULT"
+	echo "OOZIE'S JOB $NAME FAILED - $DESC"
 	exit $STATE_CRITICAL
 fi
